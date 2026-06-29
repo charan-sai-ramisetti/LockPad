ebbcf0c8c97e8958627950a580f91975dfca2276
RAMISETTI CHARAN SAI <charansairamisetti@gmail.com>
2026-06-29 14:46:47 +0530

    Update S3 bucket name for Terraform backend

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "lockpad-terraform-state-bucket"
+    bucket         = "lockpad-terraform-state-bucket-211"
     key            = "global/s3/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "lockpad-terraform-lock"
